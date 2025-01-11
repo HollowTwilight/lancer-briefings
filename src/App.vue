a992564b77f5dda3a6944957942d340479bfd8d2
HollowTwilight <95319457+HollowTwilight@users.noreply.github.com>
2025-01-10 18:10:31 -0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,4 @@
-<template>
+sion<template>
   <Header :header="this.header" />
   <div class="content-container">
     <section class="section-container" id="missions" style="width:435px; height:714px;">
@@ -116,21 +116,13 @@ export default {
           "frame": "Everest",
           "mech": "Big Money No Whammies"
         },
-        {
-          "callsign": "Sabre",
-          "alias": "Hatice Bywater",
-          "code": "462370be-bd0f-41c2-b667-cc75f3a59a96///NDL-C-DEEP-STATION//377308ad-ba23-410b-ae37-68a1fb5f8db4",
+                        {
+          "callsign": "FRIES",
+          "alias": "Illsan Medolow",
+          "code": "68pb8rt7-gi8o-v69c-ssi4-n9gtxkatso01///NDL-C-DEEP-STATION//zevl2a3v-4f4v-rbe9-7wzrl-sckz9tl125h",
           "corpro": "GMS",
           "frame": "Everest",
-          "mech": "Knife's Edge"
-        },
-        {
-          "callsign": "Peasant",
-          "alias": "Gody Gottlieb Volo",
-          "code": "7cd700cc-c990-48ed-892f-e5468de724c4///NDL-C-DEEP-STATION//a98c3e28-ad4a-4f89-bcd9-501464e960da",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Big Tractor"
+          "mech": "Scrooge"
         },
         {
           "callsign": "Liberty",
@@ -139,22 +131,6 @@ export default {
           "corpro": "GMS",
           "frame": "Everest",
           "mech": "Freedom"
-        },
-        {
-          "callsign": "Echo",
-          "alias": "Cathie Catherine Ezekiel-Dake",
-          "code": "98ca9616-044e-4f87-b89b-aae4eb3387ec///NDL-C-DEEP-STATION//6f572259-6946-41bf-931a-e0543709e892",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Loud & Proud"
-        },
-        {
-          "callsign": "Longshot",
-          "alias": "Miloo Catignani",
-          "code": "d1fdf62e-d81e-4e10-97c8-df3bc4860117///NDL-C-DEEP-STATION//5a4254aa-9fa2-42ca-a077-8f5bfd1e1ad3",
-          "corpro": "GMS",
-          "frame": "Everest",
-          "mech": "Unseen Eye"
         },
       ],
       "header": {
