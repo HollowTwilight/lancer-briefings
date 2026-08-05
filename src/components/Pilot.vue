33a74162f7eb8e845f52151834a087c0c40d4983
HollowTwilight <95319457+HollowTwilight@users.noreply.github.com>
2026-08-04 20:43:03 -0700

    update

--- src/components/Pilot.vue
+++ src/components/Pilot.vue
@@ -141,6 +141,7 @@ import longrimData from 'lancer-longrim-data'
 
 import wallflowerData from '@/assets/LCPs/wallflower-data-2.0.5'
 import dustgraveData from '@/assets/LCPs/dustgrave-data-1.4.0'
+
 /*Append the datasets within computed if your LCP has new items.
 EX:
 pilotGear() {
